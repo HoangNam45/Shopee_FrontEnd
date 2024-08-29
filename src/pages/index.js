dfd9fe5dcd56c77719db7652d061fa2ebcd2f013
Nam <nhhnam0405@gmail.com>
2024-08-30 02:26:45 +0700

    Product Management Page setup

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,10 @@
 export { default as Home } from './Home';
 export { default as Auth } from './Auth';
+
 export { default as SellerHome } from './SellerHome';
 export { default as ProductAddition } from './ProductAddition';
+export { default as ProductManagement } from './ProductManagement';
+
 export { default as ProductDetail } from './ProductDetail';
+
 export { default as ShopProfile } from './ShopProfile';
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,13 +1,16 @@
 import { DefaultLayout, HeaderWithSidebar, HeaderOnly } from '../components/Layouts';
-import { Home, Auth, SellerHome, ProductAddition, ProductDetail, ShopProfile } from '../pages';
+import { Home, Auth, SellerHome, ProductAddition, ProductDetail, ShopProfile, ProductManagement } from '../pages';
 
 const publicRoutes = [
     { path: '/', component: Home, layout: DefaultLayout },
     { path: '/login', component: Auth, layout: DefaultLayout },
     { path: '/register', component: Auth, layout: DefaultLayout },
+
     { path: '/seller', component: SellerHome, layout: HeaderWithSidebar },
+    { path: '/seller/all_product', component: ProductManagement, layout: HeaderWithSidebar },
     { path: '/seller/add_product', component: ProductAddition, layout: HeaderOnly },
     { path: '/seller/shop_profile', component: ShopProfile, layout: HeaderWithSidebar },
+
     { path: '/products/:slug', component: ProductDetail, layout: DefaultLayout },
 ];
 const privateRoutes = [];

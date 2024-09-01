3ea9ddb2bb691cd6b93a3b8dd26d07cfb661b748
Nguyen Huu Hoang Nam <nhhnam0405@gmail.com>
2024-09-01 15:07:37 +0700

    change device

--- src/services/authService.js
+++ src/services/authService.js
@@ -1,11 +1,11 @@
 import axios from 'axios';
 
 export const register = async (userData) => {
-    const response = await axios.post(`http://localhost:5000/register`, userData);
+    const response = await axios.post(`http://localhost:5000/auth/register`, userData);
     return response.data;
 };
 
 export const login = async (credentials) => {
-    const response = await axios.post(`http://localhost:5000/login`, credentials);
+    const response = await axios.post(`http://localhost:5000/auth/login`, credentials);
     return response.data;
 };

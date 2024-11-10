1eeab40c14aa039a047ef5116f86b3bad8bca1f3
Nguyen Huu Hoang Nam <nhhnam0405@gmail.com>
2024-11-10 17:57:16 +0700

    Using Date Time Picker in CreateDiscountPage

--- src/App.js
+++ src/App.js
@@ -4,68 +4,75 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { publicRoutes, privateRoutes } from './routes';
 import { DefaultLayout } from './components/Layouts';
 import PrivateAccess from './components/PrivateAccess/PrivateAccess';
+import { LocalizationProvider } from '@mui/x-date-pickers';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import dayjs from 'dayjs';
+import 'dayjs/locale/vi';
+dayjs.locale('vi');
 
 function App() {
     return (
-        <Router>
-            <div className="App">
-                <Suspense fallback={<div>Loading...</div>}>
-                    <Routes>
-                        {/* PUBLIC ROUTES */}
-                        {publicRoutes.map((route, index) => {
-                            const Page = route.component;
+        <LocalizationProvider dateAdapter={AdapterDayjs} adapterLocale="vi">
+            <Router>
+                <div className="App">
+                    <Suspense fallback={<div>Loading...</div>}>
+                        <Routes>
+                            {/* PUBLIC ROUTES */}
+                            {publicRoutes.map((route, index) => {
+                                const Page = route.component;
 
-                            let Layout = DefaultLayout;
+                                let Layout = DefaultLayout;
 
-                            if (route.layout) {
-                                Layout = route.layout;
-                            } else if (route.layout === null) {
-                                Layout = Fragment;
-                            }
+                                if (route.layout) {
+                                    Layout = route.layout;
+                                } else if (route.layout === null) {
+                                    Layout = Fragment;
+                                }
 
-                            return (
-                                <Route
-                                    key={index}
-                                    path={route.path}
-                                    element={
-                                        <Layout>
-                                            <Page />
-                                        </Layout>
-                                    }
-                                />
-                            );
-                        })}
-
-                        {/* PRIVATE ROUTES */}
-                        {privateRoutes.map((route, index) => {
-                            const Page = route.component;
-
-                            let Layout = DefaultLayout;
-
-                            if (route.layout) {
-                                Layout = route.layout;
-                            } else if (route.layout === null) {
-                                Layout = Fragment;
-                            }
-
-                            return (
-                                <Route
-                                    key={index}
-                                    path={route.path}
-                                    element={
-                                        <PrivateAccess>
+                                return (
+                                    <Route
+                                        key={index}
+                                        path={route.path}
+                                        element={
                                             <Layout>
                                                 <Page />
                                             </Layout>
-                                        </PrivateAccess>
-                                    }
-                                />
-                            );
-                        })}
-                    </Routes>
-                </Suspense>
-            </div>
-        </Router>
+                                        }
+                                    />
+                                );
+                            })}
+
+                            {/* PRIVATE ROUTES */}
+                            {privateRoutes.map((route, index) => {
+                                const Page = route.component;
+
+                                let Layout = DefaultLayout;
+
+                                if (route.layout) {
+                                    Layout = route.layout;
+                                } else if (route.layout === null) {
+                                    Layout = Fragment;
+                                }
+
+                                return (
+                                    <Route
+                                        key={index}
+                                        path={route.path}
+                                        element={
+                                            <PrivateAccess>
+                                                <Layout>
+                                                    <Page />
+                                                </Layout>
+                                            </PrivateAccess>
+                                        }
+                                    />
+                                );
+                            })}
+                        </Routes>
+                    </Suspense>
+                </div>
+            </Router>
+        </LocalizationProvider>
     );
 }
 
--- src/pages/CreateDiscountPage/index.js
+++ src/pages/CreateDiscountPage/index.js
@@ -1,12 +1,48 @@
 import styles from './CreateDiscountPage.module.scss';
 import classNames from 'classnames/bind';
 import '../../assets/styles/globalClass.scss';
+import { DateTimeRangePicker } from '@mui/x-date-pickers-pro/DateTimeRangePicker';
+import Button from '../../components/Button/Button';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPlus } from '@fortawesome/free-solid-svg-icons';
 const cx = classNames.bind(styles);
 function CreateDiscountPage() {
     return (
-        <div className={cx('board', 'create_discount_info_wrap')}>
-            <h2 className={cx('create_discount_info_header')}>Thông tin cơ bản</h2>
-        </div>
+        <>
+            <div className={cx('board', 'create_discount_info_wrap')}>
+                <h2 className={cx('board_header')}>Thông tin cơ bản</h2>
+                <div className={cx('create_discount_info_time_wrap')}>
+                    <div className={cx('create_discount_info_time')}>Thời gian khuyến mãi</div>
+                    <div className={cx('create_discount_info_time_picker')}>
+                        <DateTimeRangePicker
+                            slotProps={{ textField: { size: 'small' } }}
+                            localeText={{ start: 'Bắt đầu', end: 'Kết thúc' }}
+                            disablePast
+                        />
+                    </div>
+                </div>
+            </div>
+
+            <div className={cx('board', 'add_product_info_wrap')}>
+                <h2 className={cx('board_header')}>Thông tin cơ bản</h2>
+                <h3 className={cx('board_sub_header')}>
+                    Thêm sản phẩm vào chương trình khuyến mãi và thiết lập giá khuyến mãi.
+                </h3>
+                <Button className={cx('add_product_info_button')} primary_text medium>
+                    <FontAwesomeIcon className={cx('add_product_info_icon')} icon={faPlus} />
+                    <span>Thêm sản phẩm</span>
+                </Button>
+            </div>
+
+            <div className={cx('action_button')}>
+                <Button className={cx('action_button_item')} primary small>
+                    Xác nhận
+                </Button>
+                <Button text small>
+                    Hủy
+                </Button>
+            </div>
+        </>
     );
 }
 
